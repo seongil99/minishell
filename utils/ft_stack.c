5708a10f07b8b203b5db938b92f00d305adac7f0
Seongil Yoon <seonyoon@c3r9s5.42seoul.kr>
2024-02-17 20:03:15 +0900

    fix: norminette

--- utils/ft_stack.c
+++ utils/ft_stack.c
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 20:09:45 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/02/17 16:17:46 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/02/17 20:02:14 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,78 +31,4 @@ void	stack_del(t_stack *st, void (*f)(void *))
 	st->_top = NULL;
 	st->_btm = NULL;
 	free(st);
-}
-
-void	*stack_top(t_stack *st)
-{
-	if (!st->_top)
-		exit_err("Error: attempted to top() empty stack\n");
-	return (st->_top->data);
-}
-
-void	stack_push(t_stack *st, int n)
-{
-	t_lst	*new;
-	int		*data;
-
-	data = ft_calloc2(1, sizeof(int));
-	*data = n;
-	new = lst_new(data);
-	if (!st->_btm)
-	{
-		st->_top = new;
-		st->_btm = new;
-	}
-	else
-	{
-		st->_top->next = new;
-		new->prev = st->_top;
-		new->next = NULL;
-		st->_top = new;
-	}
-	st->size++;
-}
-
-void	stack_push_void(t_stack *st, void *data)
-{
-	t_lst	*new;
-
-	new = lst_new(data);
-	if (!st->_btm)
-	{
-		st->_top = new;
-		st->_btm = new;
-	}
-	else
-	{
-		st->_top->next = new;
-		new->prev = st->_top;
-		new->next = NULL;
-		st->_top = new;
-	}
-	st->size++;
-}
-
-void	stack_pop(t_stack *st, void (*f)(void *))
-{
-	t_lst	*t;
-
-	if (st->size == 0)
-	{
-		write(STDERR_FILENO, "Error: attempted to pop() empty stack\n", 38);
-		exit(1);
-	}
-	t = st->_top;
-	if (st->size == 1)
-	{
-		lst_del(&(st->_btm), t, f);
-		st->_top = NULL;
-		st->_btm = NULL;
-		st->size--;
-		return ;
-	}
-	st->_top = st->_top->prev;
-	lst_del(&(st->_btm), t, f);
-	st->_top->next = NULL;
-	st->size--;
 }
